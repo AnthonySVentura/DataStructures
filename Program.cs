eb462dfd0dddbc3da915449e78c7967c0893fdd8
AnthonySVentura <107430112+AnthonySVentura@users.noreply.github.com>
2022-07-05 07:31:30 -0400

    Finihsed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine(Reverse);
+
+static String Reverse(String str)
+{
+    char[] charArr = str.ToCharArray();
+    int size = charArr.Length;
+    Stack<char> stack = new Stack<char>();
+
+    int i;
+    for (i = 0; i < size; ++i)
+    {
+        stack.Push(charArr[i]);
+    }
+
+    for (i = 0; i < size; ++i)
+    {
+        charArr[i] = (char)stack.Pop();
+    }
+
+    return String.Join(" ", charArr);
+}
